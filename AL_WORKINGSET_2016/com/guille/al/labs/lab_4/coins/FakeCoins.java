d29ea3b258d2df6d44e5295a699edc9ae39a1e95
Guillermo Facundo Colunga <colunga91@gmail.com>
2016-02-26 01:36:24 +0100

    Signed-off-by: Guillermo Facundo Colunga <colunga91@gmail.com>

--- AL_WORKINGSET_2016/com/guille/al/labs/lab_4/coins/FakeCoins.java
+++ AL_WORKINGSET_2016/com/guille/al/labs/lab_4/coins/FakeCoins.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class FakeCoins {
     
     private Coins coins;
-    private int weight = 0;
+    protected int weight = 0;
     
     public FakeCoins(Coins coins) {
 	setCoins(coins);
@@ -26,16 +26,22 @@ public class FakeCoins {
     /**
      * Computes the standard weight for the coins. With O(1).
      */
-    private void calculateWeight() {
+    protected int calculateWeight() {
+	if(coins.coins.length < 3)
+	    return coins.coins[0];
 	Random r = new Random();
-	int aux1;
-	int aux2;
-	do {
+	int aux1 = 0;
+	int aux2 = 0;
+	
+	while((aux1 == aux2) || (coins.coins[aux1]!=coins.coins[aux2])) {
 	    aux1 = r.nextInt(coins.getNumberOfCoins()-1);
 	    aux2 = r.nextInt(coins.getNumberOfCoins()-1);
-	} while(aux1-aux2!=0);
+	    System.out.println(aux1 + " -- "+aux2);
+	    System.out.println(coins.coins[aux1] + " -- "+coins.coins[aux2]);
+	}
 	
 	this.weight = coins.coins[aux1];
+	return weight;
     }
     
     /**
@@ -60,16 +66,35 @@ public class FakeCoins {
     }
     
     private int findFake(int left, int right) {
-	System.out.println("Weight calculated: " + weight);
-	int middle = (right - left) / 2;
-	if(coins.weigh(left, middle, middle, right) == ScalePosition.EQUAL)
-	    System.out.println("IS THE MIDDLE");
-	else if(coins.weigh(left, middle, middle, right) == ScalePosition.LEFT)
-	    System.out.println("IS THE LEFT SIDE");
-	else if(coins.weigh(left, middle, middle, right) == ScalePosition.RIGHT)
-	    System.out.println("IS THE RIGHT SIDE");
+	//coins.prints(left, right);
+	int size = right - left + 1;
+	int middle = (right + left)/2;
+	if(size==1) {
+	   return left;
+	}
 	
-	return -1;
+	if(size%2 == 0) {
+	    if(weight(left, middle-1)==weight)
+		return findFake(middle, right);
+	    else
+		return findFake(left, middle-1);
+	    
+	} else {
+	    if(weight(left, middle-1)==weight && weight(middle+1, right)==weight) {
+		return middle;
+	    } else if(weight(left, middle-1)==weight) {
+		return findFake(middle+1, right);
+	    } else {
+		return findFake(left, middle-1);
+	    }
+	}
     }
     
+    public float weight(int left, int right) {
+	int weight = 0;
+	int size = right-left+1;
+	for(int i = left; i<=right; i++)
+	    weight += coins.coins[i];
+	return ((float)weight/size);
+    }
 }
--- AL_WORKINGSET_2016/com/guille/al/labs/lab_4/coins/FakeCoinsTest.java
+++ AL_WORKINGSET_2016/com/guille/al/labs/lab_4/coins/FakeCoinsTest.java
@@ -1,24 +1,45 @@
-package com.guille.al.labs.lab_4.coins; 
+package com.guille.al.labs.lab_4.coins;
 
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
  * JUnit Test for FakeCoins
  */
 public class FakeCoinsTest {
-	@Test
-	public void test() {
-		for (int i= 100; i<1000; i++) { //Coins in the bag (bags between 100 and 1000 coins)
-			for (int j=0; j<i; j++) { //Position of the fake coin
-				Coins bag = new Coins(i, j); //We create a bag of i coins, placing the fake coin in the position j
-				FakeCoins fakeCoins = new FakeCoins(bag); 
-				int fakePos = fakeCoins.findFake(); //Our algorithm says that the fake coin is in the position fakeCoin
-				System.out.println("Bag "+i+" with coins, fake in position: "+ fakePos + " ");
-				//Lets compare the expected result (fakePos) with the actual position we know (j)
-				assertEquals("Error because the expected result is not equal to the obtained one:" , j, fakePos);
-			}
-		}
+    @Test
+    public void test() {
+	for (int i = 9; i < 11; i++) { // Coins in the bag (bags between 100 and 1000 coins)
+	    for (int j = 0; j < i; j++) { // Position of the fake coin
+		Coins bag = new Coins(i, j); // We create a bag of i coins,
+					     // placing the fake coin in the
+					     // position j
+		FakeCoins fakeCoins = new FakeCoins(bag);
+		bag.prints(0, i - 1);
+		int fakePos = fakeCoins.findFake(); // Our algorithm says that
+						    // the fake coin is in the
+						    // position fakeCoin
+		System.out.println("Bag " + i + " with coins, fake in position: " + fakePos + " EXPECTED " + j);
+		// Lets compare the expected result (fakePos) with the actual
+		// position we know (j)
+		assertEquals("Error because the expected result is not equal to the obtained one:", j, fakePos);
+	    }
 	}
-	
+    }
+
+    @Test
+    @Ignore
+    public void weightTest() {
+	for (int i = 10; i < 100000; i++) { // Coins in the bag (bags between
+					    // 100 and 1000 coins)
+	    Coins bag = new Coins(i);
+	    FakeCoins coins = new FakeCoins(bag);
+	    int calculated = coins.calculateWeight();
+	    System.out.println("REAL: " + bag.weight + " CALCULATED: " + calculated + " size = " + i);
+	    assertEquals(bag.weight, calculated);
+	}
+    }
+
 }
