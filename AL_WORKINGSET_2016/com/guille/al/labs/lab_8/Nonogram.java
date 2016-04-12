50b4a8f0e74cdb130cb72a1168d7b5ecfff8af62
Guillermo Facundo Colunga <colunga91@gmail.com>
2016-04-12 19:04:36 +0200

    Signed-off-by: Guillermo Facundo Colunga <colunga91@gmail.com>

--- AL_WORKINGSET_2016/com/guille/al/labs/lab_8/Nonogram.java
+++ AL_WORKINGSET_2016/com/guille/al/labs/lab_8/Nonogram.java
@@ -7,68 +7,105 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Nonogram {
-    
+
     private int size;
     private List<ArrayList<Integer>> verticalRestrictions;
     private List<ArrayList<Integer>> horizontalRestrictions;
-    private int[][] board;
-    
+    private boolean[][] board;
+
     public static final String PATH_IN = "com/guille/al/files/in/lab_8/";
-    
+
+    /**
+     * Creates a nonogram of the given size.
+     * 
+     * @param size
+     *            for the nonogram, will be n height and weight.
+     */
     public Nonogram(int size) {
-	if(size > 0) {
+	if (size > 0) {
 	    this.size = size;
-	    board = new int[size][size];
+	    board = new boolean[size][size];
 	    verticalRestrictions = new ArrayList<ArrayList<Integer>>();
 	    horizontalRestrictions = new ArrayList<ArrayList<Integer>>();
 	    loadArrays();
 	}
     }
-    
+
+    /**
+     * Private method to initialize the sub-arrays for the restrictions.
+     */
     private void loadArrays() {
 	ArrayList<Integer> aux;
-	for(int i = 0; i < size; i++) {
+	for (int i = 0; i < size; i++) {
 	    aux = new ArrayList<Integer>();
 	    verticalRestrictions.add(aux);
 	    horizontalRestrictions.add(aux);
 	}
     }
-    
+
+    /**
+     * Gets the size of the nonogram.
+     * 
+     * @return the size of the nonogram as an integer.
+     */
     public int getSize() {
 	return this.size;
     }
-    
+
+    /**
+     * Gets into one array all the vertical restrictions.
+     * 
+     * @return an array containing all the vertical restrictions.
+     */
     public List<ArrayList<Integer>> getVerticalRestictions() {
 	List<ArrayList<Integer>> aux = new ArrayList<ArrayList<Integer>>();
-	for(ArrayList<Integer> arr : verticalRestrictions) {
+	for (ArrayList<Integer> arr : verticalRestrictions) {
 	    ArrayList<Integer> rest = new ArrayList<Integer>();
-	    for(Integer num : arr) {
+	    for (Integer num : arr) {
 		rest.add(num);
 	    }
 	    aux.add(arr);
 	}
 	return aux;
-    } 
-    
+    }
+
+    /**
+     * Gets into one array all the horizontal restrictions.
+     * 
+     * @return an array containing all the horizontal restrictions.
+     */
     public List<ArrayList<Integer>> getHorizontalRestictions() {
 	List<ArrayList<Integer>> aux = new ArrayList<ArrayList<Integer>>();
-	for(ArrayList<Integer> arr : horizontalRestrictions) {
+	for (ArrayList<Integer> arr : horizontalRestrictions) {
 	    ArrayList<Integer> rest = new ArrayList<Integer>();
-	    for(Integer num : arr) {
+	    for (Integer num : arr) {
 		rest.add(num);
 	    }
 	    aux.add(arr);
 	}
 	return aux;
-    } 
-    
-    public int[][] getBoard() {
+    }
+
+    /**
+     * Gets the nonogram board.
+     * 
+     * @return the nonogram board reference.
+     */
+    public boolean[][] getBoard() {
 	return this.board;
     }
-    
+
+    /**
+     * Adds a vertical restriction to the board.
+     * 
+     * @param possition
+     *            where the vertical restriction is.
+     * @param resttriction
+     *            itself.
+     */
     public void addVerticalRestriction(int pos, int rest) {
 	ArrayList<Integer> aux = verticalRestrictions.get(pos);
-	if(aux == null) {
+	if (aux == null) {
 	    aux = new ArrayList<Integer>();
 	    aux.add(rest);
 	    verticalRestrictions.add(pos, aux);
@@ -76,10 +113,18 @@ public class Nonogram {
 	    aux.add(rest);
 	}
     }
-    
+
+    /**
+     * Adds an horizontal restriction to the board.
+     * 
+     * @param possition
+     *            where the horizontal restriction is.
+     * @param resttriction
+     *            itself.
+     */
     public void addHorizontalRestriction(int pos, int rest) {
 	ArrayList<Integer> aux = horizontalRestrictions.get(pos);
-	if(aux == null) {
+	if (aux == null) {
 	    aux = new ArrayList<Integer>();
 	    aux.add(rest);
 	    horizontalRestrictions.add(pos, aux);
@@ -87,41 +132,58 @@ public class Nonogram {
 	    aux.add(rest);
 	}
     }
-    
+
+    /**
+     * NOT IMPLEMENTED YET.
+     */
     public void print() {
-	for(int i = 0; i < this.size; i++) {
-	    for(int j = 0; j < this.size; j++) {
-		
+	for (int i = 0; i < this.size; i++) {
+	    for (int j = 0; j < this.size; j++) {
+
 	    }
 	}
 	System.out.println("FAIL. NOT IMPLEMENTED YET");
     }
-    
+
+    /**
+     * Loads the data onto the system.
+     * 
+     * @format first row is the size and then will have size rows with the
+     *         horizontal restrictions and size rows with the vertical
+     *         restrictions.
+     * @param fileName,
+     *            name of the file where data is stored.
+     * @return a nonogram containing all the previous loaded data.
+     * @throws IOException
+     *             if any problem while creating the file or reading it occurs.
+     */
     @SuppressWarnings("resource")
     public static Nonogram loadData(String fileName) throws IOException {
 	Nonogram nonogram = null;
 	BufferedReader br;
 	int counter = 0;
-	br = new BufferedReader(new FileReader(PATH_IN+fileName)); // Read the specified file
+	br = new BufferedReader(new FileReader(PATH_IN + fileName)); // Read the
+								     // specified
+								     // file
 	String line = br.readLine();
 	counter++;
-	if(line != null) {
+	if (line != null) {
 	    nonogram = new Nonogram(Integer.parseInt(line));
 	}
 	line = br.readLine();
-	while(line != null) {
+	while (line != null) {
 	    counter++;
 	    String[] parts;
-	    if(counter <= nonogram.size+1) {
+	    if (counter <= nonogram.size + 1) {
 		parts = line.split(" ");
-		for(int i = 0; i < parts.length; i++) {
-		    nonogram.addHorizontalRestriction(counter-2, Integer.parseInt(parts[i]));
+		for (int i = 0; i < parts.length; i++) {
+		    nonogram.addHorizontalRestriction(counter - 2, Integer.parseInt(parts[i]));
 		}
-		
+
 	    } else {
 		parts = line.split(" ");
-		for(int i = 0; i < parts.length; i++) {
-		    nonogram.addVerticalRestriction(counter-nonogram.size-2, Integer.parseInt(parts[i]));
+		for (int i = 0; i < parts.length; i++) {
+		    nonogram.addVerticalRestriction(counter - nonogram.size - 2, Integer.parseInt(parts[i]));
 		}
 	    }
 	    line = br.readLine();
--- AL_WORKINGSET_2016/com/guille/al/labs/lab_8/NonogramTest.java
+++ AL_WORKINGSET_2016/com/guille/al/labs/lab_8/NonogramTest.java
@@ -10,18 +10,23 @@ public class NonogramTest {
     public void testLoadCases() throws IOException {
 	Nonogram nonogram = Nonogram.loadData("case1.txt");
 	assertNotEquals(null, nonogram);
-	
+	assertEquals(5, nonogram.getSize());
+
 	nonogram = Nonogram.loadData("case2.txt");
 	assertNotEquals(null, nonogram);
-	
+	assertEquals(10, nonogram.getSize());
+
 	nonogram = Nonogram.loadData("case3.txt");
 	assertNotEquals(null, nonogram);
-	
+	assertEquals(15, nonogram.getSize());
+
 	nonogram = Nonogram.loadData("case4.txt");
 	assertNotEquals(null, nonogram);
-	
+	assertEquals(20, nonogram.getSize());
+
 	nonogram = Nonogram.loadData("case5.txt");
 	assertNotEquals(null, nonogram);
+	assertEquals(25, nonogram.getSize());
     }
 
 }
